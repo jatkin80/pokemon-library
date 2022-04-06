f9547a092cc06966858beb375e2b98c2e712f949
Julie <jatkin80@ford.com>
2022-04-06 07:51:31 -0600

    update JS for better understanding of query params

---
+++ docs/index.js
@@ -0,0 +1,29 @@
+const spinner = document.querySelector(".spinner")
+const ul = document.querySelector("ul")
+
+
+function addPokemonImage(pokemon) {
+    const div = document.createElement('div')
+    div.classList.add('pokemon-listing')
+    div.innerHTML = `
+    <a href="pokemon.html?pokemon=${pokemon.name}">
+    <img src="${pokemon.sprites.front_default }"alt="${pokemon.name}"/>
+    </a>
+    `
+    ul.append(div)
+}
+const url = "https://pokeapi.co/api/v2/pokemon/?limit=50"
+fetch(url)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        const urls = parsedResponse.results.map(result => result.url)
+        const fetches = urls.map(url => fetch(url).then(response => response.json()))
+        return Promise.all(fetches)
+    })
+    .then(responses => {
+        spinner.classList.add('hidden')
+        responses.forEach(response => {
+            addPokemonImage(response)
+        })
+    })
---
+++ docs/pokemon.js
@@ -0,0 +1,43 @@
+ const url = new URL(window.location)
+ const queryString = new URLSearchParams(url.search)
+ const main = document.querySelector("main")
+
+ fetch(`https://pokeapi.co/api/v2/pokemon/${queryString.get("pokemon")}`)
+     .then(response => response.json())
+     .then(response => {
+         const name = `${ response.name }`;
+         const title = document.querySelector("title");
+         title.textContent = name;
+         const pokemonDetails = document.createElement("div")
+         pokemonDetails.classList = "pokemon-details"
+         pokemonDetails.innerHTML = `
+            <figure>
+                <img src="${response.sprites.front_shiny}" alt="${name}" />
+                <figcaption>${name}</figcaption>
+            </figure>
+            <h2>Abilities</h2>
+        `;
+         main.append(pokemonDetails);
+         const abilitiesRequests = response.abilities
+             .map(response => response.ability.url)
+             .map(url => {
+                 return fetch(url).then(response => response.json())
+             })
+         return Promise.all(abilitiesRequests)
+     }).then(responses => {
+         const ul = document.createElement("ul")
+         ul.classList = "abilities"
+         main.append(ul)
+         responses.map(response => {
+             const li = document.createElement("li")
+             li.innerHTML = `
+                <span class="ability-name">${response.name}</span>
+                <span class="ability-short-description">${response.effect_entries[1].short_effect}</span> 
+                `
+             return li;
+         }).forEach(li => {
+             ul.append(li)
+         })
+         const spinner = document.querySelector(".spinner")
+         spinner.classList.add("hidden")
+     })
