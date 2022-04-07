764716a8b8a77b12ac5dc316fb107e8422690ea2
Julie <jatkin80@ford.com>
2022-04-07 14:38:57 -0600

    main page CSS

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,16 @@
 const spinner = document.querySelector(".spinner")
 const ul = document.querySelector("ul")
 
+
 function addPokemonImage(pokemon) {
-    const div = document.createElement("div")
+    const div = document.createElement('div')
     div.classList.add('pokemon-listing')
     div.innerHTML = `
-<figure>
-<img src = "${pokemon.sprites.front_default }" alt = "${pokemon.name}" />
- <figcaption><a href="pokemon.html?pokemon=${pokemon.name}">${pokemon.name}</a></figcaption>
-</figure>
-    `
+   <figure>
+       <img src = "${pokemon.sprites.front_default }" alt = "${pokemon.name}"/>
+       <figcaption> <a href="pokemon.html?pokemon=${pokemon.name}"> ${pokemon.name} </a></figcaption>
+       </figure>
+   `
     ul.append(div)
 }
 const url = "https://pokeapi.co/api/v2/pokemon/?limit=50"
@@ -20,12 +21,6 @@ fetch(url)
         const urls = parsedResponse.results.map(result => result.url)
         const fetches = urls.map(url => fetch(url).then(response => response.json()))
         return Promise.all(fetches)
-    })
-    .then(responses => {
-        spinner.classList.add('hidden')
-        responses.forEach(response => {
-            addPokemonImage(response)
-        })
     })
     .then(responses => {
         spinner.classList.add('hidden')
