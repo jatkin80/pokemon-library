82ad4e628d5660e4b142f4ff99320115daee87d1
Julie <jatkin80@ford.com>
2022-04-11 15:29:06 -0600

    additional correction of formatting

--- site/index.js
+++ site/index.js
@@ -2,29 +2,33 @@ const spinner = document.querySelector(".spinner")
 const ul = document.querySelector("ul")
 
 
-function addPokemonImage(pokemon) {
-    const div = document.createElement('div')
-    div.classList.add('pokemon-listing')
-    div.innerHTML = `
-    <figure>
-    <img src="${pokemon.sprites.front_default }"alt="${pokemon.name}"/>
+function addPokemonImage (pokemon) {
+  const div = document.createElement("div")
+  div.classList.add("pokemon-listing")
+  const imageUrl = pokemon.sprites.front_default
+  div.innerHTML = `
+    <figure> <a href= "pokemon.html?pokemon=${pokemon.name}">
+    <img src="${imageUrl }"alt="${pokemon.name}"/>
    <figcaption>${pokemon.name}</figcaption>
+   </a>
     </figure>
     `
-    ul.append(div)
+  ul.append(div)
 }
-const url = "https://pokeapi.co/api/v2/pokemon/?limit=50"
-fetch(url)
-    .then(response => {
-        return response.json()
-    }).then(parsedResponse => {
-        const urls = parsedResponse.results.map(result => result.url)
-        const fetches = urls.map(url => fetch(url).then(response => response.json()))
-        return Promise.all(fetches)
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=50"
+fetch(apiUrl)
+  .then(response => {
+    return response.json()
+  })
+  .then(parsedResponse => {
+    const parsedUrls = parsedResponse.results.map(result => result.url)
+    const fetches = parsedUrls.map(url => fetch(url)
+      .then(response => response.json()))
+    return Promise.all(fetches)
+  })
+  .then(responses => {
+    spinner.classList.add("hidden")
+    responses.forEach(response => {
+      addPokemonImage(response)
     })
-    .then(responses => {
-        spinner.classList.add('hidden')
-        responses.forEach(response => {
-            addPokemonImage(response)
-        })
-    })
+  })
