340a60e283111f2ce8a620e00501bb8cded855c7
Julie <jatkin80@ford.com>
2022-04-11 15:20:06 -0600

    vs code hates me

--- site/index.js
+++ site/index.js
@@ -1,48 +1,30 @@
-const main = document.querySelector("main")
-const url = new URL(window.location)
-const queryString = new URLSearchParams(url.search)
 const spinner = document.querySelector(".spinner")
+const ul = document.querySelector("ul")
 
-function addPokemonImage (pokemon) {
-  const div = document.createElement("div")
-  div.classList.add("pokemon-details")
-  div.innerHTML = `
-<figure>
-    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
-    <figcaption>${pokemon.name}</figcaption>
-        </figure>
-        <h2>Abilities</h2>
+
+function addPokemonImage(pokemon) {
+    const div = document.createElement('div')
+    div.classList.add('pokemon-listing')
+    div.innerHTML = `
+    <figure>
+    <img src="${pokemon.sprites.front_default }"alt="${pokemon.name}"/>
+   <figcaption>${pokemon.name}</figcaption>
+    </figure>
     `
-  main.append(div)
+    ul.append(div)
 }
-
-function englishAbilities (arrayofObjects) {
-  const englishEffect = arrayofObjects.find(object => object.language.name === "en")
-  return englishEffect.short_effect
-}
-
-function addAbilities (response) {
-  const ul = document.createElement("ul")
-  ul.classList = ("abilities")
-  main.append(ul)
-  const li = document.createElement("li")
-  li.innerHTML = `
-<span class='ability-name'>${response.name}</span>
-<br>
-  <span class='ability-short-description'>${englishAbilities(response.effect_entries)}</span>
-  `
-  ul.append(li)
-}
-
-fetch(`https://pokeapi.co/api/v2/pokemon/${queryString.get("pokemon")}`)
-  .then(response => response.json())
-  .then(response => {
-    addPokemonImage(response)
-    const abilities = response.abilities.map(result => result.ability)
-    const urls = abilities.map(ability => ability.url)
-    const fetches = urls.map(url => fetch(url).then(response => response.json()))
-    return Promise.all(fetches).then(responses => {
-      responses.forEach(response => addAbilities(response))
-      spinner.classList.add("hidden")
+const url = "https://pokeapi.co/api/v2/pokemon/?limit=50"
+fetch(url)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        const urls = parsedResponse.results.map(result => result.url)
+        const fetches = urls.map(url => fetch(url).then(response => response.json()))
+        return Promise.all(fetches)
     })
-  })
+    .then(responses => {
+        spinner.classList.add('hidden')
+        responses.forEach(response => {
+            addPokemonImage(response)
+        })
+    })
