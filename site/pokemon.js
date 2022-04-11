1ba9e470e3183db1a38bb464250cadea063e5f47
Julie <jatkin80@ford.com>
2022-04-11 16:00:41 -0600

    final formatting

--- site/pokemon.js
+++ site/pokemon.js
@@ -6,9 +6,10 @@ const spinner = document.querySelector(".spinner")
 function addPokemonImage (pokemon) {
   const div = document.createElement("div")
   div.classList.add("pokemon-details")
+  const imageUrl = pokemon.sprites.front_default
   div.innerHTML = `
 <figure>
-    <img src="${ pokemon.sprites.front_default }" alt="${ pokemon.name }" />
+    <img src="${ imageUrl }" alt="${ pokemon.name }" />
     <figcaption>${ pokemon.name }</figcaption>
         </figure>
         <h2>Abilities</h2>
@@ -16,20 +17,22 @@ function addPokemonImage (pokemon) {
   main.append(div)
 }
 
-function englishAbilities (arrayofObjects) {
-  const englishEffect = arrayofObjects.find(object => object.language.name === "en")
+function engAbilities (engMatch) {
+  const englishEffect = engMatch
+    .find(object => object.language.name === "en")
   return englishEffect.short_effect
 }
 
-function addAbilities (response) {
+function addAbilities (enResponse) {
   const ul = document.createElement("ul")
   ul.classList = ("abilities")
   main.append(ul)
   const li = document.createElement("li")
+  const abilitiesUrl = engAbilities(enResponse.effect_entries)
   li.innerHTML = `
-<span class='ability-name'>${ response.name }</span>
+<span class='ability-name'>${ enResponse.name }</span>
 <br>
-  <span class='ability-short-description'>${ englishAbilities(response.effect_entries) }</span>
+  <span class='ability-short-description'>${ abilitiesUrl }</span>
   `
   ul.append(li)
 }
@@ -39,10 +42,11 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${ queryString.get("pokemon") }`)
   .then(response => {
     addPokemonImage(response)
     const abilities = response.abilities.map(result => result.ability)
-    const urls = abilities.map(ability => ability.url)
-    const fetches = urls.map(url => fetch(url).then(response => response.json()))
-    return Promise.all(fetches).then(responses => {
-      responses.forEach(response => addAbilities(response))
+    const parsedUrls = abilities.map(ability => ability.url)
+    const fetches = parsedUrls.map(parsedUrl => fetch(parsedUrl)
+      .then(parsedResponse => parsedResponse.json()))
+    return Promise.all(fetches).then(parsedResponses => {
+      parsedResponses.forEach(parsedResponse => addAbilities(parsedResponse))
       spinner.classList.add("hidden")
     })
   })
